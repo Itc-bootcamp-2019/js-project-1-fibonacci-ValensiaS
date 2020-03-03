0d86dca2633af28ccc77ee77b3f8c9534a71e600
Natalia <you@example.com>
2020-03-03 19:21:49 +0200

    third milestone

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,11 +1,23 @@
-document.getElementById("x").innerHTML = 15;
+let loop = document.getElementById("x").innerHTML = 12;
 
-function fibonacci(num) {
-    let arr = [0, 1];
-    for (let i = 2; i < n + 1; i++){
-          arr.push(arr[i - 2] + arr[i -1])
-        }
-       return arr[num]
-      }
+// function fibonacci(num) {
+//     let arr = [0, 1];
+//     for (let i = 2; i < n + 1; i++){
+//           arr.push(arr[i - 2] + arr[i -1])
+//         }
+//        return arr[num]
+//       }
+// let num=15;
 
-document.getElementById("y").innerHTML = arr[num];
+
+
+function fibonacci (n) {
+    var a = 0, b = 1, f = 1;
+    for(var i = 2; i <= n; i++) {
+        f = a + b;
+        a = b;
+        b = f;
+    }
+    return f;
+};
+document.getElementById("y").innerText = fibonacci(loop);
