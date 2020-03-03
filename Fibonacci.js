9ad0d4e16bec3f63f60f4f3daa131183da31a30c
Natalia <you@example.com>
2020-03-03 18:38:49 +0200

    completed milestone 2

---
+++ Fibonacci.js
@@ -0,0 +1,11 @@
+document.getElementById("x").innerHTML = 15;
+
+function fibonacci(num) {
+    let arr = [0, 1];
+    for (let i = 2; i < n + 1; i++){
+          arr.push(arr[i - 2] + arr[i -1])
+        }
+       return arr[num]
+      }
+
+document.getElementById("y").innerHTML = arr[num];
