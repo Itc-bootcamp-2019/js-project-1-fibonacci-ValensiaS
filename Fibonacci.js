565f6b351a5c3d3a39fed26a42cead8ee982198b
Natalia <you@example.com>
2020-03-09 21:48:05 +0200

    sixth milestone

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,54 +1,51 @@
 
-//milestone 4
-// //fibonacci function
-// function fibonacci(n) {
-//     let a = 0, b = 1, f = 1;
-//     if (n<=1) {
-//     return n;
-//     }
-//     for (let i = 2; i <= n; i++) {
-//         f = a + b;
-//         a = b;
-//         b = f;
-//     }
-//     return f;
-//    };
-
-// //getting the data from the user input
-//    function getInputValue(){
-//     // Selecting the input element and get its value 
-//     let inputVal = document.getElementById("x").value;
-//     return inputVal;
-//         // Displaying the value
-// }
-// //putting fibonacci function into action
-// function buttonClicked() {
-//     const n = getInputValue();
-//     console.log(n);
-//     document.getElementById("y").innerText = fibonacci(n);
-//     console.log(fibonacci(n));
-// }
-
-
-//milestone 5
-// getting the data from the user input
-function getInputValue(){
-    let inputVal = document.getElementById("x").value;
-        return inputVal;
-        }
+function getResult() {
+  document.getElementById("timer").style.visibility = "visible";
+  document.getElementById("alarm").style.visibility = "hidden";
+  document.getElementById("errorId").style.visibility = "hidden";
+  document.getElementById("y").style.visibility = "hidden";
  
-       
-    //sending data and getting response
-    function buttonClicked() {
-        const n = getInputValue();
-        fetch ("http://localhost:5050/fibonacci/" + n)
-        .then (res=>res.json())
-        .then (function(data) {
-         let answer = data.result;
+  let inputVal = document.getElementById("x").value;
+  let n = inputVal;
+  
+  if (inputVal > 50) {
+    // document.getElementById("alarm").style.visibility = "hidden";
+    document.getElementById("timer").style.visibility = "hidden";
+    document.getElementById("errorId").style.visibility = "visible";
+    document.getElementById("xButton").style.border = "1px solid #A94442";
+    document.getElementById("x").style.color = "#A94442";
+    return inputVal;
+    }else{
+        
+  fetch("http://localhost:5050/fibonacci/" + n)
+  .then(res => {
+    if (res.ok) {
+      console.log(res);
+      return res.json()
+      .then(function(data) {
+        let answer = data.result;
+        console.log(data);      
+        document.getElementById("xButton").style.border = "1px solid black";
+        document.getElementById("x").style.color = "black";
+        document.getElementById("timer").style.visibility = "hidden";
+        document.getElementById("alarm").style.visibility = "hidden";
+        document.getElementById("errorId").style.visibility = "hidden";
+        document.getElementById("y").style.visibility = "visible";
         document.getElementById("y").innerText = answer;
+ 
         console.log(answer);
-         })       
-           
-          
-        }
-    
+      });
+    } else {
+      res.text().then(text => {
+        console.log(text);
+        document.getElementById("timer").style.visibility = "hidden";
+        document.getElementById("alarm").style.visibility = "visible";
+        document.getElementById("alarm").innerHTML = "Server error: " + text;
+      });
+    }
+});
+}
+}
+
+const button = document.getElementById("in");
+button.addEventListener("click", getResult);
