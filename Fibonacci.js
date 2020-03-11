a8aec6a57d53df4932d55120b7be8b044e7d4e8d
Natalia <you@example.com>
2020-03-11 20:46:48 +0200

    seventh milestone

--- Fibonacci.js
+++ Fibonacci.js
@@ -7,7 +7,7 @@ function getResult() {
  
   let inputVal = document.getElementById("x").value;
   let n = inputVal;
-  
+
   if (inputVal > 50) {
     // document.getElementById("alarm").style.visibility = "hidden";
     document.getElementById("timer").style.visibility = "hidden";
@@ -15,7 +15,7 @@ function getResult() {
     document.getElementById("xButton").style.border = "1px solid #A94442";
     document.getElementById("x").style.color = "#A94442";
     return inputVal;
-    }else{
+    }else{  
         
   fetch("http://localhost:5050/fibonacci/" + n)
   .then(res => {
@@ -34,6 +34,7 @@ function getResult() {
         document.getElementById("y").innerText = answer;
  
         console.log(answer);
+        getAllResults ();
       });
     } else {
       res.text().then(text => {
@@ -49,3 +50,77 @@ function getResult() {
 
 const button = document.getElementById("in");
 button.addEventListener("click", getResult);
+
+
+// function addElement(fibData) { 
+//     // create a new div element 
+//     for (let i = 0; i < 10; i++){
+//     let expandingList = document.createElement("div");
+//     let expandingLine = document.createElement("hr");
+//     expandingList.innerText = "The Fibonachi of " + fibData.results[i].number 
+//     +" is "+ fibData.results[i].result+". Calculated at "+fibData.results[i].createdDate; 
+//     document.getElementById("resultAll").appendChild(expandingList);
+//     document.getElementById("resultAll").appendChild(expandingLine);
+//     expandingList.classList.add("resultAll-next");
+//     expandingLine.classList.add("resultAll-next-line");
+//   } 
+// }
+
+ 
+ 
+function getAllResults (){
+
+  fetch("http://localhost:5050/getFibonacciResults")
+  .then(res =>res.json())
+  .then(function(data){ 
+   console.log(data);
+   console.log(data.results.length);
+ 
+    let expandingListWrapper = document.createElement("div");//create wrapper for result
+    expandingListWrapper.classList.add("expandingListWrapper");//line class
+
+   for (let i = 0; i < data.results.length; i++){
+      
+
+    let expandingLine = document.createElement("hr");//create line
+    expandingLine.classList.add("resultAll-next-line");//
+
+    let expandingList = document.createElement("div");//create string of result
+    expandingList.classList.add("resultAll-next");
+
+    let expandingList1 = document.createElement("div");
+    expandingList1.classList.add("expendingList1");
+    expandingList1.innerText = "The Fibonachi of  ";
+
+    let expandingList2 = document.createElement("div");
+    expandingList2.classList.add("expendingList2");
+    expandingList2.innerText = data.results[i].number;
+
+    let expandingList3 = document.createElement("div");
+    expandingList3.classList.add("expendingList1");
+    expandingList3.innerText = " is ";
+
+    let expandingList4 = document.createElement("div");
+    expandingList4.classList.add("expendingList2");
+    expandingList4.innerHTML = data.results[i].result;
+
+    let expandingList5 = document.createElement("div");
+    expandingList5.classList.add("expendingList1");
+    expandingList5.innerText = " . Calculated at ";
+
+    let expandingList6 = document.createElement("div");
+    expandingList6.classList.add("expendingList2");
+    expandingList6.innerHTML = new Date(data.results[i].createdDate);
+
+
+    document.getElementById("resultAll").appendChild(expandingListWrapper);
+    // document.getElementById("resultAll").appendChild(expandingLine);
+    // document.getElementById("resultAll").appendChild(expandingList);
+    
+    expandingListWrapper.append(expandingLine, expandingList);
+    expandingList.append(expandingList1,expandingList2,expandingList3,expandingList4,expandingList5,expandingList6);
+    
+   }
+  })
+};
+   
